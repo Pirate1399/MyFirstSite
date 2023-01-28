e7d24568b6a6ac1580877b27f6b1d0d41042c942
Pirate1399 <samovole@gmail.com>
2023-01-28 17:35:10 +0300

    2 practica

--- mysite/settings.py
+++ mysite/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'common',
+    'cart',
 ]
 
 MIDDLEWARE = [
@@ -122,6 +123,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- mysite/urls.py
+++ mysite/urls.py
@@ -17,9 +17,10 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('', include('common.urls')),
     path('', include('testapp.urls')),
+    path('cart/', include('cart.urls')),
 ]
 
 
